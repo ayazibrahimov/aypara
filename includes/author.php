a39a1145ddb0a5da42120a12e8bb5eedeac64e27
ayaz ibrahimov1~ <ayaz.ibrahimov.1998@mail.ru>
2023-03-01 14:38:55 +0400

    data

--- includes/author.php
+++ includes/author.php
@@ -1,15 +1,15 @@
 <div>
-    <div class="section-title pb-0">
-        <h4 class="title left-line">Yazarlar</h4>
+    <div class="section-title section-title--parent">
+        <h4 class="title section-title--mini">Yazarlar</h4>
     </div>
     <div class=''>
         <div class="author__box d-flex  align-item-center">
             <div class="author__avatar--box">
                 <img src="assets/img//imagedata/avatar.jpg" alt="">
             </div>
-            <div class="ms-3">
-                <h4>Hesen Ozturk</h4>
-                <a href=""><p class="author__text">6-lı masalı 20gündə 6 Şubat öncəsində</p></a>
+            <div class="ms-3 py-2">
+                <h5>Hesen Ozturk</h5>
+                 <p class="author__text">6-lı masalı 20gündə 6 Şubat öncəsində</p>
             </div>
         </div>
         <hr>
@@ -17,9 +17,9 @@
             <div class="author__avatar--box">
                 <img src="assets/img//imagedata/avatar.jpg" alt="">
             </div>
-            <div class="ms-3">
-                <h4>Hesen Ozturk</h4>
-                <a href=""><p class="author__text">6-lı masalı 20gündə 6 Şubat öncəsində</p></a>
+            <div class="ms-3 py-2">
+                <h5>Hesen Ozturk</h5>
+                 <p class="author__text">6-lı masalı 20gündə 6 Şubat öncəsində</p>
             </div>
         </div>
         <hr>
@@ -27,9 +27,9 @@
             <div class="author__avatar--box">
                 <img src="assets/img//imagedata/avatar.jpg" alt="">
             </div>
-            <div class="ms-3">
-                <h4>Hesen Ozturk</h4>
-                <a href=""><p class="author__text">6-lı masalı 20gündə 6 Şubat öncəsində</p></a>
+            <div class="ms-3 py-2">
+                <h5>Hesen Ozturk</h5>
+                 <p class="author__text">6-lı masalı 20gündə 6 Şubat öncəsində</p>
             </div>
         </div>
         <hr>
@@ -37,9 +37,9 @@
             <div class="author__avatar--box">
                 <img src="assets/img//imagedata/avatar.jpg" alt="">
             </div>
-            <div class="ms-3">
-                <h4>Hesen Ozturk</h4>
-                <a href=""><p class="author__text">6-lı masalı 20gündə 6 Şubat öncəsində</p></a>
+            <div class="ms-3 py-2">
+                <h5>Hesen Ozturk</h5>
+                 <p class="author__text">6-lı masalı 20gündə 6 Şubat öncəsində</p>
             </div>
         </div>
     </div>
